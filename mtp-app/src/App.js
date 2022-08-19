5fa90fdb6f211ca9232733d13b3d6f80eae70e6a
agnawoussama <agnawoussama@gmail.com>
2022-08-19 20:52:37 +0100

    changes

--- mtp-app/src/App.js
+++ mtp-app/src/App.js
@@ -3,7 +3,7 @@ import Aside from './components/Aside/Aside';
 import Content from './components/Content/Content';
 import { BrowserRouter} from "react-router-dom";
 
-function App() {  /*h*/
+function App() {  /*a*/
   return (
     <BrowserRouter>
       <div className="App m-0 p-0 flex">
